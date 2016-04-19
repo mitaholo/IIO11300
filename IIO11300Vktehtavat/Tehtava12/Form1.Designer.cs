7fd9b337f0df3c9f7240c2c62e28eb28d2e6d9fd
Mikko Holopainen <mitaholo@gmail.com>
2016-04-19 13:50:33 +0300

    Tehtävä 12c

---
+++ IIO11300Vktehtavat/Tehtava12/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Tehtava12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.cbLanguage = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnNewUser = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblCurrentType = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            resources.ApplyResources(this.tbUsername, "tbUsername");
+            this.tbUsername.Name = "tbUsername";
+            // 
+            // lblUsername
+            // 
+            resources.ApplyResources(this.lblUsername, "lblUsername");
+            this.lblUsername.Name = "lblUsername";
+            // 
+            // lblPassword
+            // 
+            resources.ApplyResources(this.lblPassword, "lblPassword");
+            this.lblPassword.Name = "lblPassword";
+            // 
+            // tbPassword
+            // 
+            resources.ApplyResources(this.tbPassword, "tbPassword");
+            this.tbPassword.Name = "tbPassword";
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            resources.GetString("cbType.Items")});
+            resources.ApplyResources(this.cbType, "cbType");
+            this.cbType.Name = "cbType";
+            this.cbType.SelectedIndexChanged += new System.EventHandler(this.cbType_SelectedIndexChanged);
+            // 
+            // cbLanguage
+            // 
+            this.cbLanguage.FormattingEnabled = true;
+            this.cbLanguage.Items.AddRange(new object[] {
+            resources.GetString("cbLanguage.Items"),
+            resources.GetString("cbLanguage.Items1")});
+            resources.ApplyResources(this.cbLanguage, "cbLanguage");
+            this.cbLanguage.Name = "cbLanguage";
+            this.cbLanguage.SelectedIndexChanged += new System.EventHandler(this.cbLanguage_SelectedIndexChanged);
+            // 
+            // btnOk
+            // 
+            resources.ApplyResources(this.btnOk, "btnOk");
+            this.btnOk.Name = "btnOk";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnNewUser
+            // 
+            resources.ApplyResources(this.btnNewUser, "btnNewUser");
+            this.btnNewUser.Name = "btnNewUser";
+            this.btnNewUser.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            resources.ApplyResources(this.btnCancel, "btnCancel");
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // lblType
+            // 
+            resources.ApplyResources(this.lblType, "lblType");
+            this.lblType.Name = "lblType";
+            // 
+            // lblCurrentType
+            // 
+            resources.ApplyResources(this.lblCurrentType, "lblCurrentType");
+            this.lblCurrentType.Name = "lblCurrentType";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblCurrentType);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNewUser);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.cbLanguage);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.tbUsername);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.ComboBox cbLanguage;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnNewUser;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblCurrentType;
+    }
+}
+
---
+++ IIO11300Vktehtavat/Tehtava12/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
+
+namespace Tehtava12
+{
+    public partial class Form1 : Form
+    {
+        private int language;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbLanguage.SelectedIndex = 0;
+            cbType.SelectedIndex = 0;
+        }
+
+        private void ChangeLanguage(string language)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+                resources.ApplyResources(ctrl, ctrl.Name, new CultureInfo(language));
+            }
+        }
+
+        private void cbType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblCurrentType.Text = cbType.Text;
+        }
+
+        private void cbLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbLanguage.SelectedIndex)
+            {
+                case 0:
+                    ChangeLanguage("fi-FI");
+                    break;
+                case 1:
+                    ChangeLanguage("en-GB");
+                    break;
+                default:
+                    ChangeLanguage("fi-FI");
+                    break;
+            }
+        }
+    }
+}
